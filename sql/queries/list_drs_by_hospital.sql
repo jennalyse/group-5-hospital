17a9d60a801ef4feeace0e2d1591b697b25bff4f
jennalyse <93606894+jennalyse@users.noreply.github.com>
2024-12-31 16:53:45 +0000

    Update list_drs_by_hospital.sql

--- sql/queries/list_drs_by_hospital.sql
+++ sql/queries/list_drs_by_hospital.sql
@@ -1,13 +1,21 @@
 -- 1. Print a list of all doctors at a particular hospital
 
--- Use hospital database
-USE hospitals_db;
+-DELIMITER //
 
--- Query to print all doctors based at a particular hospital
--- Replace 'Particular Hospital Name' with the desired hospital name when running the script
-SELECT d.first_name, d.second_name AS last_name, d.address
-FROM DOCTORS d
-JOIN HOSPITALS h ON d.hospital_id = h.hospital_id
-WHERE h.hospital_name = 'Royal London Hospital';
+-- Create a stored procedure to list doctors by hospital name
+CREATE PROCEDURE ListDoctorsByHospital(IN hospital_name VARCHAR(255))
+BEGIN
+    -- Validate that the hospital exists
+    IF (SELECT COUNT(*) FROM HOSPITALS WHERE hospital_name = hospital_name) = 0 THEN
+        SELECT "Hospital not found." AS message;
+        LEAVE PROCEDURE;
+    END IF;
 
--- End of script
+    -- Query to list doctors based on the hospital name
+    SELECT d.first_name, d.second_name AS last_name, d.address
+    FROM DOCTORS d
+    JOIN HOSPITALS h ON d.hospital_id = h.hospital_id
+    WHERE h.hospital_name = hospital_name;
+END //
+
+DELIMITER ;
