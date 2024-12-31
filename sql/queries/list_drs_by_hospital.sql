29ef47ebae942ebbf7a8f8c7ee1c5686845a5888
jennalyse <93606894+jennalyse@users.noreply.github.com>
2024-12-31 16:42:00 +0000

    Update and rename print_list_all_drs_particular_hospital.sql to list_drs_by_hospital.sql

---
+++ sql/queries/list_drs_by_hospital.sql
@@ -0,0 +1,13 @@
+-- 1. Print a list of all doctors at a particular hospital
+
+-- Use hospital database
+USE hospitals_db;
+
+-- Query to print all doctors based at a particular hospital
+-- Replace 'Particular Hospital Name' with the desired hospital name when running the script
+SELECT d.first_name, d.second_name AS last_name, d.address
+FROM DOCTORS d
+JOIN HOSPITALS h ON d.hospital_id = h.hospital_id
+WHERE h.hospital_name = 'Particular Hospital Name';
+
+-- End of script
